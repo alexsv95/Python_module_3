f160f73a3393720e006d41b6fcd63392ad2bd792
alexsv95 <a.sosnov95@gmail.com>
2024-09-06 16:09:44 +0300

    Done

--- module_3_4.py
+++ module_3_4.py
@@ -1,1 +1,13 @@
 # Задача "Однокоренные"
+
+def single_root_words(root_word, *other_words):
+    same_words = [] # Создание пустого списка
+    for i in other_words:
+        if root_word.lower() in i.lower() or i.lower() in root_word.lower(): # Если слово из параметра root_word содержится в одном из слов списка other_word и наоборот,
+            same_words.append(i) # то слово из списка other_word добавляется в список same_words
+    return same_words # Функция возвращает список same_words
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
